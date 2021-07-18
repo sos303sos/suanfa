a638a590a7a065d3ea957f5ea7a61520cb12340a
sunhuabin <95129024@qq.com>
2021-07-18 14:51:47 +0800

    merge复制

---
+++ src/main/java/sort/MergeCopy.java
@@ -0,0 +1,15 @@
+package sort;
+
+/**
+ * 最简单的归并实现，采用另外开辟空间来实现
+ */
+public class MergeCopy extends Template {
+    public MergeCopy(Comparable[] data) {
+        super(data);
+    }
+
+    @Override
+    public void sort() {
+
+    }
+}
